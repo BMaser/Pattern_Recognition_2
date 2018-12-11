08605f52e85ac46a046e741ffdcc490ee9123249
HugoPlatzer <hugoplatzer@aon.at>
2018-12-11 04:32:00 +0100

    more memory-efficient generation,training

--- Word2Vec/ExtractSentences/skip_grams.py
+++ Word2Vec/ExtractSentences/skip_grams.py
@@ -1,52 +1,33 @@
 #!/usr/bin/python3 -u
 
-import sys, argparse, re, random
+import sys, argparse, re, random, itertools
 
-def keepProbabilities(chunks, vocabulary):
-  occurrences = [0] * len(vocabulary)
-  for chunk in chunks:
-    for word in chunk:
-      if word in vocabulary:
-        occurrences[vocabulary[word]] += 1
-  total = sum(occurrences)
-  relative = [o / float(total) for o in occurrences]
-  keepProbs = []
-  for i in range(len(vocabulary)):
-    if relative[i] > 0.0:
-      v = (((relative[i] / 0.001)**0.5) + 1.0) * 0.001 / relative[i]
+def keepProbabilities(words, vocabulary):
+  occurrences = {word : 0 for word in vocabulary}
+  for word in words:
+    if word in vocabulary:
+      occurrences[word] += 1
+  total = sum(occurrences[word] for word in vocabulary)
+  relative = {word : occurrences[word] / float(total) for word in vocabulary}
+  keepProbs = {}
+  for word in vocabulary:
+    if relative[word] > 0.0:
+      v = (((relative[word] / 0.001)**0.5) + 1.0) * 0.001 / relative[word]
       v = min(v, 1.0)
-      keepProbs.append(v)
+      keepProbs[word] = v
     else:
-      keepProbs.append(0.0)
+      keepProbs[word] = 1.0
   return keepProbs
 
-def filterWords(words, vocabulary, keepProbs=None):
-  filtered = []
-  for w in words:
-    if w in vocabulary:
-      if keepProbs is not None:
-        if random.random() < keepProbs[vocabulary[w]]:
-          filtered.append(w)
-        else:
-          filtered.append(None)
-      else:
-        filtered.append(w)
-    else:
-      filtered.append(None)
-  return filtered
-
-def generateSkipGrams(words, vocabulary, windowSize):
-  skipGrams = []
-  for i in range(len(words)):
-    currentWord = words[i]
-    if currentWord is not None:
+def generateSkipGrams(words, windowSize):
+  for i, word in enumerate(words):
+    if word is not None:
       lowerLimit = max(0, i - windowSize)
       upperLimit = min(len(words) - 1, i + windowSize)
       for iOther in range(lowerLimit, upperLimit + 1):
         otherWord = words[iOther]
         if iOther != i and otherWord is not None:
-          skipGrams.append((currentWord, otherWord))
-  return skipGrams
+          yield (word, otherWord)
 
 def main():
   parser = argparse.ArgumentParser()
@@ -62,21 +43,20 @@ def main():
                        "comma" : "\n|!|\\?|\\.|,|:|;"}
   
   with open(args.vocabularyFile) as f:
-    vocabulary = f.read().splitlines()
-  vocabulary = {v:i for i, v in enumerate(vocabulary)}
+    vocabulary = set(f.read().splitlines())
   text = sys.stdin.read().lower()
   if args.separators != "none":
     chunks = re.split(separatorPatterns[args.separators], text)
   else:
     chunks = [text]
-  chunks = [re.findall("[A-Za-z]+", c) for c in chunks]
   if args.subsampling:
-    keepProbs = keepProbabilities(chunks, vocabulary)
-    chunks = [filterWords(c, vocabulary, keepProbs) for c in chunks]
+    words = (m.group(0) for c in chunks for m in re.finditer("[A-Za-z]+", c))
+    keepProbs = keepProbabilities(words, vocabulary)
   else:
-    chunks = [filterWords(c, vocabulary) for c in chunks]
-  skipGrams = [sg for c in chunks
-                  for sg in generateSkipGrams(c, vocabulary, args.windowSize)]
+    keepProbs = {w : 1.0 for w in vocabulary}
+  wordChunks = ((m.group(0) for m in re.finditer("[A-Za-z]+", c)) for c in chunks)
+  wordChunks = [[(w if w in vocabulary and random.random() < keepProbs[w] else None) for w in wc] for wc in wordChunks]
+  skipGrams = (sg for wc in wordChunks for sg in generateSkipGrams(wc, args.windowSize))
   for sg in skipGrams:
     print(sg[0] + " " + sg[1])
 
--- Word2Vec/TrainingModel/train.py
+++ Word2Vec/TrainingModel/train.py
@@ -4,6 +4,7 @@ from keras.callbacks import ModelCheckpoint
 from keras.optimizers import SGD, RMSprop, Adagrad
 from keras.initializers import Zeros, RandomNormal, RandomUniform
 from keras.backend import set_session
+from math import ceil
 import tensorflow as tf
 import numpy as np
 import argparse
@@ -13,14 +14,26 @@ import os
 def load_vocabulary(file_name):
   with open(file_name, "r") as f:
     lines = [l.strip().lower() for l in f.readlines() if not l.isspace()]
-  return lines
+  return {word : i for i, word in enumerate(lines)}
 
-def load_pairs(training_file, vocabulary):
+def number_of_training_pairs(training_file):
   with open(training_file, "r") as f:
-    lines = [l.strip().lower() for l in f.readlines() if not l.isspace()]
-  word_pairs = [l.split(" ") for l in lines]
-  index_pairs = [(vocabulary.index(w1), vocabulary.index(w2)) for w1, w2 in word_pairs]
-  return index_pairs
+    return sum(1 for l in f if not l.isspace())
+
+def generate_training_matrices(training_file, vocabulary, batch_size, cache_size=100000):
+  while True:
+    pairs_cache = []
+    with open(training_file, "r") as f:
+      while True:
+        if len(pairs_cache) == 0:
+          lines = [f.readline().strip() for i in range(cache_size)]
+          word_pairs = [l.split(" ") for l in lines if len(l) > 0]
+          pairs_cache = [(vocabulary[w1], vocabulary[w2]) for w1, w2 in word_pairs]
+          if len(pairs_cache) == 0:
+            break
+        index_pairs, pairs_cache = pairs_cache[:batch_size], pairs_cache[batch_size:]
+        input_matrix, output_matrix = prepare_input_output_matrix(index_pairs, len(vocabulary))
+        yield (input_matrix, output_matrix)
 
 def prepare_input_output_matrix(index_pairs, vocabulary_size):
   input_matrix = np.zeros((len(index_pairs), vocabulary_size), np.byte)
@@ -30,54 +43,41 @@ def prepare_input_output_matrix(index_pairs, vocabulary_size):
     output_matrix[i][pair[1]] = 1
   return input_matrix, output_matrix
 
-def train_network(input_matrix, output_matrix, hidden_dimensions, epochs, batch_size, file_path):
-  #~ config = tf.ConfigProto(intra_op_parallelism_threads=8, inter_op_parallelism_threads=8,
-                          #~ allow_soft_placement=True, device_count = {'CPU': 1})
-  #~ session = tf.Session(config=config)
-  #~ set_session(session)
-  vector_size = input_matrix.shape[1]
-  if os.path.isfile(file_path):
-    model = load_model(file_path)
-    model.compile("adam", loss="categorical_crossentropy")
+def train_network(generator, input_dim, hidden_dim, steps_per_epoch, epochs, learn_rate, model_file):
+  initializer = "glorot_uniform"
+  optimizer = SGD(lr=learn_rate)
+  loss = "categorical_crossentropy"
+  if os.path.isfile(model_file):
+    model = load_model(model_file)
   else:
     model = Sequential()
-    #initializer = RandomUniform(minval=-0.0001, maxval=0.0001)
-    initializer = "glorot_uniform"
-    #~ initializer = "random_u"
-    #~ initializer = RandomNormal(stddev=0.1)
-    #~ optimizer = ["sgd", "rmsprop", "adagrad", "adadelta", "adam", "adamax", "nadam"][0]
-    optimizer = SGD(lr=1.0)
-    #~ optimizer = 
-    model.add(Dense(hidden_dimensions, input_dim=vector_size, activation="linear", use_bias=False, kernel_initializer=initializer))
-    model.add(Dense(vector_size, activation="softmax", use_bias=False, kernel_initializer=initializer))
-    model.compile(optimizer=optimizer, loss="categorical_crossentropy")
-  checkpoint = ModelCheckpoint(file_path, monitor='val_loss', verbose=0,
+    model.add(Dense(hidden_dim, input_dim=input_dim, activation="linear", use_bias=False, kernel_initializer=initializer))
+    model.add(Dense(input_dim, activation="softmax", use_bias=False, kernel_initializer=initializer))
+  model.compile(optimizer=optimizer, loss=loss)
+  checkpoint = ModelCheckpoint(model_file, monitor='val_loss', verbose=0,
                                save_best_only=False, save_weights_only=False,
                                mode='auto', period=1)
-  model.fit(input_matrix, output_matrix, epochs=epochs,
-            batch_size=batch_size, callbacks=[checkpoint])
+  model.fit_generator(generator, steps_per_epoch=steps_per_epoch, epochs=epochs, callbacks=[checkpoint])
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("-v", "--vocabularyFile",
-                      default="../ExtractSentences/ef_top1000.txt")
+                      default="../ExtractSentences/wikipedia_vocabulary.txt")
   parser.add_argument("-t", "--trainingFile",
-                      default="../ExtractSentences/skipgrams/news_10k.txt")
+                      default="../ExtractSentences/skipgrams/text8_ss.txt")
   parser.add_argument("-m", "--modelFile",
                       default="keras_model")
   parser.add_argument("-d", "--embeddingDimensions", type=int, default=128)
   parser.add_argument("-b", "--batchSize", type=int, default=32)
   parser.add_argument("-e", "--epochs", type=int, default=1000)
+  parser.add_argument("-l", "--learnRate", type=float, default=1.0)
   args = parser.parse_args()
   
   vocabulary = load_vocabulary(args.vocabularyFile)
-  index_pairs = load_pairs(args.trainingFile, vocabulary)
-  #~ shuffle(index_pairs)
-  #~ index_pairs = index_pairs[:100000]
-  #~ index_pairs = index_pairs[:1]
-  input_matrix, output_matrix = prepare_input_output_matrix(index_pairs, len(vocabulary))
-  train_network(input_matrix, output_matrix, args.embeddingDimensions,
-                args.epochs, args.batchSize, args.modelFile)
+  steps_per_epoch = int(ceil(number_of_training_pairs(args.trainingFile) / args.batchSize))
+  generator = generate_training_matrices(args.trainingFile, vocabulary, args.batchSize)
+  train_network(generator, len(vocabulary), args.embeddingDimensions, steps_per_epoch,
+                args.epochs, args.learnRate, args.modelFile)
 
 if __name__ == "__main__":
   main()
